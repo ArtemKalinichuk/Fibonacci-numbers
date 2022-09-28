3c9b2df32c110c4e4c51f06eef84c47fb16e46e9
Artem Kalinichuk <artemkalinichuk@gmail.com>
2022-09-28 14:34:39 +0300

    Added Fibonacci made with recursion

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ def Fibonaci(n, arr: list):
         arr.append(arr[index] + arr[index + 1])
 
 
+def Fibonacci_Rec(n):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+
+    else:
+        return Fibonacci_Rec(n - 1) + Fibonacci_Rec(n - 2)
+
+
 def main():
-    result = [0, 1]
-    Fibonaci(9,result)
-    print(result)
+    print(Fibonacci_Rec(9))
+
 
 if __name__ == "__main__":
     main()
